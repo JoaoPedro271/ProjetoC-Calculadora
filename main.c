491dfb2edef0f8fd96a7dd7e4f36980b6e75c047
Joao Soares <Joao Pedro Soares Almeida>
2023-10-16 21:41:59 -0300

    Algumas alterções

--- main.c
+++ main.c
@@ -1,64 +1,51 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <locale.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+int main() {
+    double num1, num2, resultado;
+    char operador;
 
-int main(int argc, char *argv[]) {
-	
-	
-     int num1;
-     int num2;
-     int result;
-     int opcao;
-    
    
-    printf("Digite um valor ");
-    scanf("%d",&num1);
-    printf("Digite outro valor ");
-    scanf("%d",&num2);
-    printf("O que voce deseja fazer?  1(som) 2(sub) 3(multi) e 4(div): ");
-    scanf("%d", &opcao);
-   
+    printf("Digite o primeiro numero: ");
+    scanf("%lf", &num1);
+
     
+    while (getchar() != '\n');
+
     
-    switch(opcao) {
-    	
+    printf("Escolha uma operacao (+, -, *, /): ");
+    scanf("%c", &operador);
+
+    
+    printf("Digite o segundo numero: ");
+    scanf("%lf", &num2);
+
    
-    	
-    	case 1:
-    		
-    		result = num1 + num2;
-			printf("Resultado: %d \n", result);	
-			
-    	break;
-    	
-    	case 2:
-    		
-    		result = num1 - num2;
-    		printf("Resultado: %d \n", result);	
-    		
-    	break;
-    	
-    	case 3:
-    		
-    		result = num1 * num2;
-    		printf("Resultado: %d \n", result);	
-    		
-    	break;
-    	
-    	case 4:
-    		
-    		result = num1 / num2;
-    		printf("Resultado: %d \n", result);	
-    		
-    	break;
-  	
+    switch (operador) {
+        case '+':
+            resultado = num1 + num2;
+            break;
+        case '-':
+            resultado = num1 - num2;
+            break;
+        case '*':
+            resultado = num1 * num2;
+            break;
+        case '/':
+            if (num2 != 0) {
+                resultado = num1 / num2;
+            } else {
+                printf("Erro: Divisao por zero!\n");
+                return 1; 
+            }
+            break;
+        default:
+            printf("Operação invalida.\n");
+            return 1; 
     }
-    
-    
-   
-    
-	return 0;
-	
+
+  
+    printf("Resultado: %lf\n", resultado);
+
+    return 0; 
 }
+
